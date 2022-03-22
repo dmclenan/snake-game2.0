5e687575552186eaf2cf0ae3d5eb00103d47dec2
Dondre Mclenan <d.mclenan@gmail.com>
2022-03-22 18:31:27 -0400

    update snake game

---
+++ game.js
@@ -0,0 +1,25 @@
+let lastRenderTime = 0
+const SNAKE_SPEED = 1
+
+function main(currentTime) {
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+    console.log('Render')
+    lastRenderTime = currentTime
+
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+
+}
+
+function draw() {
+    
+}
